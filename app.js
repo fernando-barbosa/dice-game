698edec76e9d02e761ed0cb41ee086eb110a89fa
Fernando Barbosa <fernandobsjunior@gmail.com>
2017-04-26 17:17:24 -0300

    Update with global score and winner logic

--- app.js
+++ app.js
@@ -27,22 +27,41 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next player turn
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+    
+    // Update de UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // Check if the player won the game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+    // Next player turn
+    nextPlayer();
+    }
+});
 
-
-
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
