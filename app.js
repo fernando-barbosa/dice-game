ebe57cdef45cec6681020e8ed4d3139fad454648
Fernando Barbosa <fernandobsjunior@gmail.com>
2017-04-26 11:12:47 -0300

    Update in app.js, with current player score.

--- app.js
+++ app.js
@@ -13,14 +13,16 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
-document.querySelector('.btn-roll').addEventListener('click', function(){
+document.querySelector('.btn-roll').addEventListener('click', function() {
    
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -31,8 +33,23 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
     
     // 3. Update the round score IF the rolled number was NOT a 1
-    
-    
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player turn
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
 
 
